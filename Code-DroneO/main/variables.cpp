d63361127fa71da5d4c90633fa997bcf41d3e49a
Alexis Nadeau <alexis.nadeau@inrs.ca>
2023-07-24 11:25:07 -0400

    Formatting

--- Code-DroneO/main/variables.cpp
+++ Code-DroneO/main/variables.cpp
@@ -2,8 +2,8 @@
 #include "variables.hpp"
 
 bool stop_loop = false;  // Define the global varible with an initial value.
-int positionplateau = 0; 
+int positionPlateau = 0; 
 void printGlobal() {
   Serial.println(stop_loop);
-  Serial.println(positionplateau);
+  Serial.println(positionPlateau);
 }
