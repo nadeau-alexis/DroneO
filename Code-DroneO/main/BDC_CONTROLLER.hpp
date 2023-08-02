99a889a98dbed1d700b6ee899ed6fe2c87bfe663
Alexis Nadeau <alexis.nadeau@inrs.ca>
2023-08-02 11:28:38 -0400

    Add includes in BDC_CONTROLLER header file

--- Code-DroneO/main/BDC_CONTROLLER.hpp
+++ Code-DroneO/main/BDC_CONTROLLER.hpp
@@ -1,6 +1,8 @@
 #ifndef BDC_CONTROLLER_hpp
 #define BDC_CONTROLLER_hpp
 #include <SoftwareSerial.h>
+#include <Encoder.h>
+#include <PID_v1.h>
 
 
 void treuilUnroll(int target_nbTurns, PID &PIDObject, Encoder &EncoderObject, int hallSensor, SoftwareSerial &HC12object, String HC12String_)
